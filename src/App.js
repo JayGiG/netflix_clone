103ae60cb488ac02e7a3a620171fe6903b044a73
Janiele Nogueira <njaniele@yahoo.com>
2020-08-24 21:35:06 -0300

    projeto criado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import tmdb from './tmdb';
+import MovieRow from './components/MovieRow'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  //qnd a página for carregada executa a função q estiver dentro do useEffect
+  useEffect(() => {
+    const loadAll = async () => {
+      //pegando a lista total
+      let list = await tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+
+  return(
+    <div className='page'>
+      
+      <section className='lists'>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+
     </div>
   );
-}
 
-export default App;
+}
