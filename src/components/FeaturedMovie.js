1655e4d3f70620a9d4f560136507609b40c07b3d
Janiele Nogueira <njaniele@yahoo.com>
2020-08-24 23:17:10 -0300

    imagem de destaque definida

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './FeaturedMovie.css';
 
-export default () => {
+export default ({item}) => {
     return (
-        <div>
-            Filmes em destaque
-        </div>
+        <section className='featured' style={{
+            backgroundSize:'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+
+        }}>
+            
+            <div className='featured--vertical'>
+                <div className='featured--horizontal'>
+                    <div className='featured--name'>{item.original_name}</div>
+                    <div className='featured--info'>{item.original_name}
+                        <div className='featured--points'>{item.vote_average} pontos</div>
+                        <div className='featured--year'>2099</div>
+    <div className='featured--seasons'>{item.number_of_seasons} temporadas{item.number_of_seasons !== 1 ? 's' : '' }</div>
+                    </div>
+
+                    <div className='featured--name'>{item.original_name}</div>
+                    <div className='featured--name'>{item.original_name}</div>
+                    <div className='featured--name'>{item.original_name}</div>
+
+                </div>
+            </div>
+
+
+            {/* <div>{item.original_name}</div> */}
+        </section>
+        
     )
 }
 
